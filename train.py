bde4255d3208aea3bfe0d3c45ebfac294154fac8
1Wesleyyou <yuchenyou76@gmail.com>
2024-01-25 17:42:01 +0100

    feat: training function added

---
+++ train.py
@@ -0,0 +1,44 @@
+from preload import train_loader
+from model import AgeGenderDetectNN
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+model = AgeGenderDetectNN()
+# 损失函数定义
+age_criterion = nn.L1Loss()  # 这里的使用的是MAE，或者说L1损失函数
+gender_criterion = nn.BCELoss()  # 二元判断用二元交叉熵损失
+# 优化器统一使用 Adam
+age_optimizer = optim.Adam(model.parameters(), lr=0.001)
+gender_optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+epoch_num = 10
+
+for epochs in range(epoch_num):
+    model.train()
+    for idx, batch in enumerate(train_loader):
+        input_image = batch['image_path']
+
+        label = batch['label']
+        label_age = label[:, 0]
+        label_gender = label[:, 1]
+        # 默认是 long，解释器不开心了
+        label_age = label_age.float()
+        label_gender = label_gender.float()
+
+        age_optimizer.zero_grad()
+        gender_optimizer.zero_grad()
+
+        age_output, gender_output = model(input_image)
+        age_loss = age_criterion(age_output, label_age)
+        gender_loss = gender_criterion(gender_output, label_gender)
+
+        age_loss.backward()
+        gender_loss.backward()
+
+        age_optimizer.step()
+        gender_optimizer.step()
+        print(idx)
+
+        if idx % 100 == 1:
+            print(f'case [{epochs + 1}/10], loss of age = {age_loss.item()}, loss of gender = {gender_loss.item()}')
