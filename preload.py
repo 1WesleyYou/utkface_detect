fb205c47a31c0a88fc1594b19447f89a9ce2f908
1Wesleyyou <yuchenyou76@gmail.com>
2024-01-25 16:05:46 +0100

    fix: preload type error; dataset label error

--- preload.py
+++ preload.py
@@ -4,20 +4,11 @@ from torch.utils.data import DataLoader, Dataset, random_split
 import torchvision.transforms as transforms
 from PIL import Image
 
-from enum import Enum
-
-
-# 枚举表示性别
-class Gender(Enum):
-    MALE = 0
-    FEMALE = 1
-
-
 # 定义 transform 内容
 transform = transforms.Compose([
     transforms.Resize([256, 256]),  # 重新设定图像大小
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5], std=[0.5])
+    # transforms.Normalize(mean=[0.5], std=[0.5])
 ])
 
 
@@ -36,20 +27,21 @@ class ImageDataset(Dataset):
             age_r = filename.find('_')
             model_age = filename[:age_r]
             rest_name = filename[age_r + 1:]
-            model_gender = rest_name[:rest_name.find('_')]
-            data.append({'image_path': os.path.join(f'dataset/train/part1/{filename}'), 'label_age': model_age,
-                         'label_gender': model_gender})  # 获取 label
+            model_gender = int(rest_name[:rest_name.find('_')])
+            data.append({'image_path': os.path.join(f'dataset/train/part1/{filename}'),
+                         'label': (int(model_age), model_gender)})  # 获取 label
 
         # 训练集读取
         for filename in os.listdir('dataset/train/part2'):
             age_r = filename.find('_')
             model_age = filename[:age_r]
             rest_name = filename[age_r + 1:]
-            model_gender = rest_name[:rest_name.find('_')]
+            model_gender = int(rest_name[:rest_name.find('_')])
             # print(f"age: {model_age}, gender: {model_gender}")
             data.append({'image_path': os.path.join(f'dataset/train/part2/{filename}'),
-                         'label': (model_age, model_gender)})  # 获取 label 保存为一个元组
+                         'label': (int(model_age), model_gender)})  # 获取 label 保存为一个元组
 
+        # print(data[1000]['label'])
         return data
 
     def __len__(self):
@@ -57,8 +49,9 @@ class ImageDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
+        print(self.data[idx])
         img_path = self.data[idx]['image_path']
-        label = torch.tensor([self.data[idx]['label']])
+        label = torch.tensor(self.data[idx]['label'])
         image = Image.open(img_path).convert('L')
 
         if self.transform:
